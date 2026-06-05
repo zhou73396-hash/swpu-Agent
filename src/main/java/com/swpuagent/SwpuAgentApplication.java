70c63e195f6395a3e571a2dac515b5859b7633a0
zhoulong <zhou73396@gmail.com>
2026-06-05 16:38:34 +0800

    feat:added source codes

---
+++ src/main/java/com/swpuagent/SwpuAgentApplication.java
@@ -0,0 +1,13 @@
+package com.swpuagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwpuAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwpuAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swpuagent/SwpuAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swpuagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwpuAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
